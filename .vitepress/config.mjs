1d8e8001d1c9d23adf439652271c3fc9ae15f989
Kayano <Kayano04@outlook.jp>
2025-05-04 01:18:52 +0900

    new updata

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -7,8 +7,8 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Home', link: '/' }
+      // { text: 'Examples', link: '/markdown-examples' }
     ],
 
     sidebar: [
@@ -47,7 +47,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/KayanoLiam' }
     ]
   }
 })
